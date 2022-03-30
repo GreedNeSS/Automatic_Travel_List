ab4f3291baee118bdb5545652647a8d1897845df
GreedNeSS <ruslan199126@gmail.com>
2022-03-30 21:47:25 +0300

    Added IRepository

---
+++ Automatic_Sheet_DAL/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatic_Sheet_DAL
+{
+    internal interface IRepository
+    {
+        void Init();
+        TripEntity GetRandomUniqTrip(int vehicleCount);
+    }
+}
